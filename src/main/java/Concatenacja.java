5a9828fd4afa97a5737ef0750b3b227bc20a114a
Daniel Szlicht <daniel.szlicht25@gmail.com>
2024-12-10 14:16:06 +0100

    konkatenacja

---
+++ src/main/java/Concatenacja.java
@@ -0,0 +1,11 @@
+public class Concatenacja {
+    public static void main(String[] args) {
+        String produkty = "Kupiłeś: ";
+
+        produkty += "Mleko";
+        produkty += " ";
+        produkty += "Ołówek";
+        produkty += " ";
+        System.out.println(produkty);
+    }
+}
